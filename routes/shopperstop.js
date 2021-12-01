57790516f8d0f8121d908d7a16689b672630c53e
cheipuri <89273883+cheipuri@users.noreply.github.com>
2021-12-01 00:40:37 -0600

    update1

--- routes/shopperstop.js
+++ routes/shopperstop.js
@@ -1,13 +1,23 @@
 var express = require('express'); 
 const shopperstop_controlers= require('../contorllers/shopperstop'); 
 var router = express.Router(); 
+// A little function to check if we have an authorized user and continue on 
+//or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
  
 /* GET shopperstops */ 
 router.get('/', shopperstop_controlers.shopperstop_view_all_Page );
 router.get('/create', shopperstop_controlers.shopperstop_create_Page); 
 router.get('/detail', shopperstop_controlers.shopperstop_view_one_Page);  
 /* GET create update page */ 
-router.get('/update', shopperstop_controlers.shopperstop_update_Page); 
+router.get('/update', secured, shopperstop_controlers.shopperstop_update_Page); 
 router.get('/delete', shopperstop_controlers.shopperstop_delete_Page); 
  
 module.exports = router; 
